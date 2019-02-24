6e45bca5bb3ffc697d9257dc9e09ddf98fe25b02
d-mv <d.dash.mv@gmail.com>
2019-02-24 16:36:25 +0200

    Schema is done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_120913) do
+ActiveRecord::Schema.define(version: 2019_02_24_134248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "checkpoints", force: :cascade do |t|
+    t.bigint "race_id"
+    t.float "lat"
+    t.float "lon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_checkpoints_on_race_id"
+  end
+
+  create_table "kits", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_kits_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "race_id"
+    t.float "lat"
+    t.float "lon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_locations_on_race_id"
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "race_id"
+    t.integer "duration"
+    t.integer "points"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_participants_on_race_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
+  create_table "races", force: :cascade do |t|
+    t.string "title"
+    t.time "start_date"
+    t.string "category"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_races_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +75,24 @@ ActiveRecord::Schema.define(version: 2019_02_24_120913) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
+    t.string "profile_type"
+    t.integer "age"
+    t.string "nationality"
+    t.string "occupation"
+    t.integer "points"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "checkpoints", "races"
+  add_foreign_key "kits", "users"
+  add_foreign_key "locations", "races"
+  add_foreign_key "locations", "users"
+  add_foreign_key "participants", "races"
+  add_foreign_key "participants", "users"
+  add_foreign_key "races", "users"
 end
