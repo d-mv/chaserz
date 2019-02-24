7236bc79088824ca243339633bedeb89b401541a
d-mv <d.dash.mv@gmail.com>
2019-02-24 17:06:02 +0200

    Access locked

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -1,2 +1,3 @@
 class CheckpointsController < ApplicationController
+  before_action :authenticate_user!
 end
--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -1,2 +1,3 @@
 class KitsController < ApplicationController
+  before_action :authenticate_user!
 end
