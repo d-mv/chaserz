1c7464f3e581c76a3fb852cb5164e56d5f53cf0e
roni talvi <vtalvi@gmail.com>
2019-02-25 12:41:39 +0200

    kit controller

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -1,2 +1,52 @@
 class KitsController < ApplicationController
+  before_action :set_kit, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @kit = Kit.new
+
+  end
+
+  def create
+    @kit = Kit.new(kit_params)
+    @kit.user = current_user
+
+    if @kit.save!
+      redirect_to root_path ##
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @kit.update(kit_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kit.destroy
+    redirect_to root ##
+  end
+
+  private
+
+  def set_kit
+    @kit = Kit.find(params[:id])
+
+  end
+
+
+
+  def kit_params
+    params.require(:kit).permit(:name, :description, :photo)
+  end
 end
