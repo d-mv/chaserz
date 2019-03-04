f2c31267657569383875706009d0368915fd13af
d-mv <d.dash.mv@gmail.com>
2019-03-04 13:30:05 +0200

    redis as per heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,13 +2,20 @@ $redis = Redis.new
 
 url = ENV["REDISCLOUD_URL"]
 
-if url
-  Sidekiq.configure_server do |config|
-    config.redis = { url: url }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: url }
-  end
-  $redis = Redis.new(:url => url)
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
+
+
+
+
+# if url
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: url }
+#   end
+
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: url }
+#   end
+#   $redis = Redis.new(:url => url)
+# end
