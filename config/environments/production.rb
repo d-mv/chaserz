29c764ce65c0b6a42bfb0168c825360fc25be7da
d-mv <d.dash.mv@gmail.com>
2019-03-04 13:52:23 +0200

    redis as per heroku manual

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  # Redis
+config.web_socket_server_url = "wss://chaserz-test-app.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://chaserz-test-app.herokuapp.com', 'http://chaserz-test-app.herokuapp.com']
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
