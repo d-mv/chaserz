085fe3fb45061f38027221e5d580ee8ac073b948
d-mv <d.dash.mv@gmail.com>
2019-03-05 09:52:12 +0200

    revised Apply Now button and logic, correct button, pointing to races

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,12 +2,10 @@ class ParticipantsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @participant = Participant.new(race_id: params[:race_id])
-    @participant.user_id = current_user.id
-    if @participant.save!
-      redirect_to race_path(params[:race_id])
-    else
-      redirect_to :race ##
-    end
+    Participant.create(
+      race_id: params[:race_id],
+      user_id: current_user.id
+    )
+    redirect_to race_path(params[:race_id])
   end
 end
